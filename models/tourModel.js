9d09f1d42b6733f5e05e8ed8a31aace29615a4a0
Danielvo <daavo.1002@gmail.com>
2021-12-08 18:10:32 -0600

    08 Query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -60,7 +60,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -87,6 +91,19 @@ tourSchema.pre('save', function(next) {
 //   console.log(doc);
 //   next();
 // });
+
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
