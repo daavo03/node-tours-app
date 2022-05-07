fd3d7c8e3b04b454394ad894950a300b177d9b66
Danielvo <daavo.1002@gmail.com>
2022-05-06 20:42:28 -0500

    Populating Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,6 +34,23 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// Populate the tours in the Review documents
+reviewSchema.pre(/^find/, function(next) {
+  // The populate process always happens in a query. This populate replaces the ID with the actual data
+  this.populate({
+    // Name of the field to replace
+    path: 'tour',
+    // To show or not certain fields
+    select: 'name'
+    // Populate the users in the Review documents
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
