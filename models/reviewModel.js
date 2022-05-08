6ca8d196eb80633494bc21d318febac2ec0abfb5
Danielvo <daavo.1002@gmail.com>
2022-05-08 14:41:08 -0500

    Calculating Average Rating on Tours when Creating Review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -51,6 +52,52 @@ reviewSchema.pre(/^find/, function(next) {
   });
 
   next();
+});
+
+// Storing summary of a related dataset on the main dataset.
+// In our app example is to store the average rating and the number of ratings on each tour, so that we don't have to query
+//the reviews and calculate averages each time we query for all Tours. Useful overview page front-end we want to show a summary
+//of the reviews (number of ratings and average) of a tour each time a new review is added/updated/deleted
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  // In order to do calculation, we use aggregation(which is called on a Model) pipeline, in static method "this" points to the current model
+  const stats = await this.aggregate([
+    // First select all the reviews that belong to the current tour that was passed in as argument
+    {
+      // First stage is match stage
+      $match: { tour: tourId }
+    },
+    {
+      // Calculate statics themselves
+      $group: {
+        // In the group phase first field we need to specify is the ID,
+        //then the common field all the docs have in common that we want to group by which is the tour
+        _id: '$tour',
+        // The number of ratings we add 1 for each tour that was match in the previous step
+        nRating: { $sum: 1 },
+        // We want to calculate the average from the "rating" field from the Review Model
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  // See if our stats is working
+  console.log(stats);
+
+  // Persisting the statics in to each Tour document
+  //We need to find the current tour and then update it
+  await Tour.findByIdAndUpdate(tourId, {
+    // Object of the data that we actually want to update
+    //The stats are in an array, so we need to go to the 1st position of that array and from there we get the "nRating" prop
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating
+  });
+};
+
+// Calling the stats from a middleware, we use post instead of pre because all the docs are already saved in the DB,
+//and it's great time to do the calc with all the reviews already and store that result on the Tour
+//In this kind of middleware the "this" keyword points to the document that is currently being saved.
+reviewSchema.post('save', function() {
+  // this points to the current document and the constructor is basically the model who created that document
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
