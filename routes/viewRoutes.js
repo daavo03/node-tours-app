2626873885d2bfa8585e76e486f0cfd75b9c979d
Danielvo <daavo.1002@gmail.com>
2022-05-10 21:56:20 -0500

    Building the Login Screen

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 // Creating route to access the template
 router.get('/', viewsController.getOverview);
 router.get('/tour/:slug', viewsController.getTour);
+router.get('/login', viewsController.getLoginForm);
 
 module.exports = router;
