f02c78c36869613f51789885ccaa22c6d4685840
Danielvo <daavo.1002@gmail.com>
2022-05-13 03:32:30 -0500

    Finishing the Bookings API

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,7 +4,7 @@ const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 // const AppError = require('../utils/appError');
-// const factory = require('./handlerFactory');
+const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get currently booked tour in our DB
@@ -62,3 +62,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   // Securing the url, redirecting the app removing the querystring
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,7 +4,22 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.protect);
+
 // This route doesn't follow REST, this route only for the client to get a checkout session
-router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession);
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
